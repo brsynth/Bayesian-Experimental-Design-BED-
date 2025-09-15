25196db7a55f9f5ea9f3e4bb8e79e2a048d816d9
Thi-Ngoc-An Hoang <hoangngocan0112@gmail.com>
2025-09-15 16:25:31 +0200

    calculated log_prob for each design

--- BED.py
+++ BED.py
@@ -8,7 +8,7 @@ from cobra import Configuration
 from cobra.io import read_sbml_model
 from library.parallel import fit_cobra
 Configuration().solver = "glpk"
-
+import jax.numpy as jnp
 import bayesflow as bf
 import matplotlib.pyplot as plt
 
@@ -80,13 +80,14 @@ result = solver_fba(**sample)
 # print(f"Sample: {sample}")
 # print(f"Result: {result}")
 
-inference_variables = variable_media + [f"design_{name}" for name in variable_media] + ["y_cobra"]
+training_variables = variable_media + [f"design_{name}" for name in variable_media] + ["y_cobra"]
 
 simulator = bf.simulators.make_simulator([prior_sampling, solver_fba])
 adapter = (
     bf.adapters.Adapter()
     .convert_dtype("float64", "float32")
-    .concatenate(inference_variables, into="inference_variables")
+    .concatenate(variable_media, into="inference_variables")
+    .concatenate([f"design_{name}" for name in variable_media], into="inference_conditions")
     # .concatenate(["y_cobra"], into="summary_variables")
 )
 
@@ -103,7 +104,7 @@ for col in variable_media:
     df_reduced[design_col] = df_reduced[col].apply(lambda x: 0 if x == 0 else (1 if 0 < x < 1 else x))
 
 
-df_reduced = df_reduced[inference_variables].dropna()
+df_reduced = df_reduced[training_variables].dropna()
 n_experiment = len(df_reduced)
 training_data = {
     k: np.array(v).reshape(-1, 1) if np.array(v).ndim == 1 else np.array(v)
@@ -114,10 +115,10 @@ history = workflow.fit_offline(
         training_data,  # Train data is a dictionary with values as 2 dimensions arrays 
         epochs=epochs,
         batch_size=batch_size,
-        inference_variables=inference_variables,
+        inference_variables=variable_media,
     )
 f = bf.diagnostics.plots.loss(history)
-plt.show()
+# plt.show()
 
 # model_path = f"Models/BED_model_{size}.h5"
 # os.makedirs(os.path.dirname(model_path), exist_ok=True)
@@ -132,7 +133,7 @@ exp_Y = exp_data.iloc[:, -1]
 
 design_list = exp_X[variable_media].drop_duplicates().reset_index(drop=True)
 n_designs = len(design_list)
-n_sample_MC = 1000
+n_sample_MC = 500
 prior_sample = prior_sampling(variable_media, n=n_sample_MC)
 
 def apply_design_mask(prior_sample: dict, design: pd.Series) -> dict:
@@ -142,15 +143,26 @@ def apply_design_mask(prior_sample: dict, design: pd.Series) -> dict:
     """
     masked = {}
     for col in prior_sample:
-        masked[col] = np.array(prior_sample[col]) * design[col]
+        res = np.array(prior_sample[col]) * design[col]
+        masked[col] = res.reshape(-1, 1)
     return masked
 
+mean_list = []
+std_list = []
 
 for i in range(n_designs):
     design = design_list.iloc[i]
     masked_prior = apply_design_mask(prior_sample, design)
-    design_dict = {f"design_{col}": design[col] for col in variable_media}
+    design_dict = {f"design_{col}": np.repeat(design[col], n_sample_MC).reshape(-1, 1) for col in variable_media}
+
     masked_prior.update(design_dict)
-    log_prob = workflow.log_prob(masked_prior)
-    print(f"Design {i+1}/{n_designs}: {np.mean(log_prob)}")
+    log_prob = workflow.log_prob(data=masked_prior)
+    print(f"Design {i+1}/{n_designs}: {np.mean(log_prob)} , {np.std(log_prob)}")
+    mean_list.append(jnp.mean(log_prob))
+    std_list.append(jnp.std(log_prob))
+    
+
+
+
+
 
